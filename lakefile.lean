cc2dfb6b4080a03774e1dbf8441ca47bd72614cf
Rob Lewis <rob.y.lewis@gmail.com>
2023-01-19 22:25:27 -0500

    updates

--- lakefile.lean
+++ lakefile.lean
@@ -3,7 +3,7 @@ open Lake DSL
 
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "d1e6d8643531bd36f7c7bb612413decb7cb07ede"
-require autograder from git "https://github.com/robertylewis/cs22-lean-autograder" @ "0d3734d33d53e1a34b98cb66edd63816fc69bc3f"
+require autograder from git "https://github.com/robertylewis/cs22-lean-autograder" @ "1c9c67a3d0c3d5d5b159c37e3146323d3b6f4bb1"
 
 package «brown-cs22» {
   -- add package configuration options here

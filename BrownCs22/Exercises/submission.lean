706a35f7a3df2b3342f46d061f08d1f101d1c03a
qcfu <qcfu@bu.edu>
2023-10-03 17:09:35 -0400

    sorry

--- BrownCs22/Exercises/submission.lean
+++ BrownCs22/Exercises/submission.lean
@@ -5,22 +5,13 @@ import BrownCs22.Demos.Readme
 
 /- 4 points -/
 theorem problem1 {a b : ℚ} (h1 : a - b = 4) (h2 : a * b = 1) :
-    (a + b) ^ 2 = 20 :=
-  calc (a + b) ^ 2 = (a - b) ^ 2 + 4 * (a * b) := by ring
-  _ = 4 ^ 2 + 4 * 1 := by rw [h1, h2]
-  _ = 20 := by ring
+    (a + b) ^ 2 = 20 := sorry
 
 /- 2 points -/
-theorem problem2 {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2) : a ≥ 0 := by
-  cases' h with b hb
-  calc a = b ^ 2 := hb
-  _ ≥ 0 := by apply sq_nonneg
+theorem problem2 {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2) : a ≥ 0 := sorry
 
 theorem you_might_use_this_theorem_in_your_answer :
-    1 + 1 = 2 := by
-  norm_num
+    1 + 1 = 2 := sorry
 
 /- 4 points -/
-theorem problem3 : ∃ n : ℤ, 12 * n = 84 := by
-  existsi 7
-  norm_num
+theorem problem3 : ∃ n : ℤ, 12 * n = 84 := sorry
